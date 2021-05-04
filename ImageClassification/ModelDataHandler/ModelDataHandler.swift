e9bcbda94f09f8a440d5db0af34aadb8a2459ce8
ValentinRicher <valentin.jean.richer@gmail.com>
2021-05-04 10:07:05 +0200

    add edamame branding

--- ImageClassification/ModelDataHandler/ModelDataHandler.swift
+++ ImageClassification/ModelDataHandler/ModelDataHandler.swift
@@ -34,9 +34,10 @@ typealias FileInfo = (name: String, extension: String)
 
 /// Information about the MobileNet model.
 enum MobileNet {
-//  static let modelInfo: FileInfo = (name: "mobilenet_quant_v1_224", extension: "tflite")
-  static let modelInfo: FileInfo = (name: "model_ft16", extension: "tflite")
-  static let labelsInfo: FileInfo = (name: "classes", extension: "txt")
+    //  static let modelInfo: FileInfo = (name: "mobilenet_quant_v1_224", extension: "tflite")
+    //  static let labelsInfo: FileInfo = (name: "labels", extension: "txt")
+      static let modelInfo: FileInfo = (name: "model_quant_int8", extension: "tflite")
+      static let labelsInfo: FileInfo = (name: "classes", extension: "txt")
 }
 
 /// This class handles all data preprocessing and makes calls to run inference on a given frame
